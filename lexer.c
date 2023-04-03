c1d25f9276bf2a238d147c26fcfae62b3352daeb
pserrano <pedserra.99@gmail.com>
2023-04-03 12:58:38 +0200

    Tema parentesis/parser

--- lexer.c
+++ lexer.c
@@ -4,35 +4,18 @@
 typedef struct s_parser
 {
 	char 	*line; // Esta linea va a contener 
-	t_list	**lista_comandos; // Para guardar cada comando en su lista
-	t_list *lexer; // Esta seria la tabla que contiene todos los datos del lexer
-	int 	num_comd; // Numero de comandos
+	t_list *lexer; // Esta seria la tabla que contiene todos los datos del lexer // Numero de comandos
 
 }				t_parser;
 
 void	init_parser(t_parser *parser)
 {
-	parser->line = (char *)malloc(sizeof(char *));
+	parser->line = (char *)malloc(sizeof(char ));
 	parser->line = "";
-	parser->lista_comandos = NULL;
-}
-
-void	copiar_en_array(t_parser *parser, char **array_aux, int num_palabras, int i, int ancla)
-{
-	int aux;
-	array_aux[num_palabras -1] =(char *)malloc(sizeof(char*));
-	aux = 0;
-	while(ancla < i)
-	{
-		array_aux[num_palabras - 1][aux] = parser->line[ancla];
-		aux++;
-		ancla++;
-	}
-	array_aux[num_palabras - 1][aux] = '\0';
-	//añadir a la lista el array_aux[num_palabras]
 }
 
 // Esta funcion me guarda la palabra en un array temporal y me devuelve el nuevo ancla
+/*
 int		condition_space(t_parser *parser, int ancla, int i, char **array_aux, int num_palabras)
 {
 	copiar_en_array(parser, array_aux, num_palabras, i, ancla);
@@ -59,87 +42,102 @@ int condition_comillas(t_parser *parser, int i, char **array_aux, int num_palabr
 	return (i);
 
 }
-
-//me lo guardara sin los parentesis
-int condition_parentesis(t_parser *parser, int i, char **array_aux, int num_palabras)
+*/
+/*
+char**	copiar_en_array(t_parser *parser, char **array_aux, int num_palabras, int i, int ancla)
 {
-	int aux_parent;
-	int ancla;
+	int aux;
 
-	aux_parent = 0;
-	ancla = i;
-	while(parser->line[i] != ')' || aux_parent != 0)
+	aux = 0;
+	aux = i - ancla;
+	printf("Numero de letras del array: %d\n", aux);
+	array_aux[num_palabras -1] =(char *)malloc(sizeof(char)*aux + 1);
+	aux = 0;
+	while(ancla <= i)
+	{
+		array_aux[num_palabras - 1][aux] = parser->line[ancla];
+		aux++;
+		ancla++;
+	}
+	array_aux[num_palabras - 1][aux] = '\0';
+	printf("AQUI HACE LLA COPIA Y La IMPRIMIMOS: %s\n", array_aux[num_palabras - 1]);
+	return (array_aux);
+	//añadir a la lista el array_aux[num_palabras]
+}
+*/
+int condition_parentesis(t_parser *parser, int i)
+{
+	int aux_abierto;
+	int aux_cerrado;
+
+	aux_abierto = 1;
+	aux_cerrado = 0;
+	while(aux_abierto != aux_cerrado)
 	{
 		if (parser->line[i] == '(')
-		{
-			aux_parent++;
-		}
-		if (parser->line[i] == ')' && aux_parent > 0)
-		{
-			aux_parent--;
-		}
+			aux_abierto++;
+		if (parser->line[i] == ')')
+			aux_cerrado++;
 		i++;
 	}
 	i--;
-	copiar_en_array(parser, array_aux, num_palabras, i, ancla);
-	i = i++;
 	return (i);
 }
 
+int	saltar_espace(char *line, int i)
+{
+	while(line[i] == ' ' || line[i] == '\t')
+		i++;
+	return(i);
+}
 
-int		separar_codigo(t_parser *parser)
+int separar(t_parser *parser)
 {
 	int i;
 	int ancla;
-	char **array_aux;
-	int num_palabras;
+	int parent;
 
 	i = 0;
-	num_palabras = 0;
-	array_aux =(char **)malloc(sizeof(char**));
-	while(parser->line[i] == ' ' || parser->line[i] == '\t')
-		i++;
+	ancla = 0;
 	while(parser->line[i])
 	{
-		if (parser->line[i] == '(')
+		if(parser->line[i] == '|' || parser->line[i] == '>' || parser->line[i] == '<' || parser->line[i] == '&')
 		{
-			if(parser->line[ft_strlen(parser->line[i])] == ')')
-				i++;
-			else
+			if(ancla != i)
+				ft_lstadd_back(&parser->lexer, ft_lstnew(ft_substr(parser->line, ancla, i - ancla)));
+			ancla = i;
+			if(parser->line[i] == parser->line[i + 1])
 			{
+				ft_lstadd_back(&parser->lexer, ft_lstnew(ft_substr(parser->line, ancla, 2)));
 				i++;
-				num_palabras++;
-				ancla = condition_parentesis(parser, i, array_aux, num_palabras);
-				i = ancla;
-			}
-		else if (parser->line[i] == ')')
-			i++;
-		else if (parser->line[i] == ' ' || parser->line[i] == '\t')
-			i++;
-		else if (parser->line[i] == '|' || parser->line[i] == '>' || parser->line[i] == '<' || parser->line[i] == '&') //añadir ||   y &&
-		{
-			num_palabras++;
-			if (parser->line[i + 1] == '|')
-			{
-				array_aux[num_palabras -1] =(char *)malloc(sizeof(char*));
-				array_aux[num_palabras - 1][0] = parser->line[i];
-				array_aux[num_palabras - 1][0] = parser->line[i];
 			}
 			else
-			{
-				array_aux[num_palabras -1] =(char *)malloc(sizeof(char*));
-				ancla++;
-				array_aux[num_palabras - 1][0] = parser->line[i];
-			}
+				ft_lstadd_back(&parser->lexer, ft_lstnew(ft_substr(parser->line, ancla, 1)));
+			i++;
+			i = saltar_espace(parser->line, i);
+			ancla = i;
+		}
+		else if (parser->line[i] == '(')
+		{
+			i++;
+			ancla = i;
+			i = condition_parentesis(parser, i);
+			ft_lstadd_back(&parser->lexer, ft_lstnew(ft_substr(parser->line, ancla, i - ancla)));
+			i++;
+			i = saltar_espace(parser->line, i);
+			ancla = i;
+			i--;
 		}
 		i++;
 	}
-	ancla = i;
-	while(parser->line[i])
+	if (parser->line[i] != ')')
 	{
+		ft_lstadd_back(&parser->lexer, ft_lstnew(ft_substr(parser->line, ancla, i - ancla)));
+	}
+
+	return (0);
 }
-
-
+/*
 // Esta funcion contara el numero de palabras que hay para separar
 int		contar_palabras(t_parser *parser)
 {
@@ -149,11 +147,11 @@ int		contar_palabras(t_parser *parser)
 	int num_palabras;
 
 	i = 0;
-	num_palabras = 0;
 	array_aux =(char **)malloc(sizeof(char**));
 	while(parser->line[i] == ' ' || parser->line[i] == '\t')
 		i++;
 	ancla = i;
+	num_palabras = 1;
 	while(parser->line[i])
 	{
 		if (parser->line[i] == ' ' || parser->line[i] == '\t')
@@ -180,11 +178,16 @@ int		contar_palabras(t_parser *parser)
 			{
 				array_aux[num_palabras -1] =(char *)malloc(sizeof(char*));
 				array_aux[num_palabras - 1][0] = parser->line[i];
+				i++;
 				array_aux[num_palabras - 1][0] = parser->line[i];
+				ancla++;
 			}
 			array_aux[num_palabras -1] =(char *)malloc(sizeof(char*));
-			ancla++;
 			array_aux[num_palabras - 1][0] = parser->line[i];
+			i++;
+			while(parser->line[i] == ' ' || parser->line[i] == '\t')
+				i++;
+			ancla = i;
 		}
 		else if (parser->line[i] == '$')
 		{
@@ -202,6 +205,7 @@ int		contar_palabras(t_parser *parser)
 	return(num_palabras);
 }
 
+*/
 
 int main(int argc, char **argv)
 {
@@ -210,27 +214,30 @@ int main(int argc, char **argv)
 	int i;
 	int len;
 	char *line;
+	t_list *list;
 	
 	i = 0;
 	len = 0;
-	parser = (t_parser*)malloc(sizeof(t_parser));
-	init_parser(parser);
-	line = (char*)malloc(sizeof(char*));
 	while (argv[1][len])
 		len++;
-	line[i] = argv[1][0];
-	i++;
-	printf("%c", line[1]);
-	while (i < len)
+	parser = (t_parser*)malloc(sizeof(t_parser));
+	init_parser(parser);
+	line = (char*)malloc(sizeof(char)*len + 1);
+	while (i <= len)
 	{
 		line[i] = argv[1][i];
 		i++;
 	}
 	line[i] = '\0';
 	parser->line = line;
-	num_palabras = contar_palabras(parser);
-	printf("%s", parser->line);
-	printf("%d", num_palabras);
+	num_palabras = separar(parser);
+	list = parser->lexer;
+	while(list)
+	{
+		printf("%s\n", list->content);
+		list = list->next;
+	}
+	free(line);
 	return (0);
 
 }
